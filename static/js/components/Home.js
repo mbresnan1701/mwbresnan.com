50f8cbcde83a4d0834409d714ad7e56e58dfe2c9
Matt Bresnan <bresnan.mw@gmail.com>
2016-06-08 21:59:43 -0700

    lots of style changes

--- static/js/components/Home.js
+++ static/js/components/Home.js
@@ -36,12 +36,9 @@ class Home extends React.Component {
   render() {
     return (
       <Row>
-        <Col lg={8} lgOffset={2} md={10} mdOffset={1}>
-          <h3>Latest Blog Entries:</h3>
-        </Col>
         {this.renderRecent()}
         <Col lg={8} lgOffset={2} md={10} mdOffset={1}>
-          <a href="/blog">More Blog Entries</a>
+          <a className="morebloglink" href="/blog">More Blog Entries</a>
         </Col>
       </Row>
     );
